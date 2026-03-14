edaf64845b1dfe74e1ebf156520228954b99236f
KingJayan <jayanp0202@gmail.com>
2026-03-14 15:14:16 -0500

    ui/ux: switch to hsla, css vars, base typography, a11y focus&motion

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -65,8 +65,8 @@ export default {
         "paper-pattern": "url(\"data:image/svg+xml,%3Csvg width='100' height='100' viewBox='0 0 100 100' xmlns='http://www.w3.org/2000/svg'%3E%3Cfilter id='noise'%3E%3CfeTurbulence type='fractalNoise' baseFrequency='0.8' numOctaves='3' stitchTiles='stitch'/%3E%3C/filter%3E%3Crect width='100%25' height='100%25' filter='url(%23noise)' opacity='0.05'/%3E%3C/svg%3E\")",
       },
       boxShadow: {
-        "paper": "4px 4px 0px 0px rgba(0,0,0,1)",
-        "paper-hover": "6px 6px 0px 0px rgba(0,0,0,1)",
+        "paper":       "4px 4px 0px 0px hsl(0, 0%, 0%)",
+        "paper-hover": "6px 6px 0px 0px hsl(0, 0%, 0%)",
       },
       animation: {
         "wiggle": "wiggle 1s ease-in-out infinite",
